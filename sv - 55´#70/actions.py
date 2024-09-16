c3ac4f0701835aa6b6f47b3143e0b0ef262ed1fa
vatxz <76906734+vatxz@users.noreply.github.com>
2024-09-16 09:25:38 -0600

    Update actions.py

--- sv - 55´#70/actions.py
+++ sv - 55´#70/actions.py
@@ -10,16 +10,13 @@ def eat_food():
    
 def hole_down(should_down):
     if should_down:
-       box = pg.locateOnScreen("imgs/barraverde.png", confidence=0.6)
+       box = pg.locateOnScreen("imgs/cuerdasubir.png", confidence=0.8)
        if box:
            x, y = pg.center(box)
-           pg.sleep(1)
-           pg.press("space")
-           pg.sleep(2)
+           pg.moveTo(x, y)
            pg.press("f4")
-           pg.sleep(4)
-           pg.press("f4")
-
+           pg.click()
+           pg.sleep(5)
 
 def hole_up(should_up, img_anchor, plus_x, plus_y):
     if should_up:
@@ -30,16 +27,23 @@ def hole_up(should_up, img_anchor, plus_x, plus_y):
            pg.click()
            pg.sleep(5)
 
-def check_status(name, delay, x, y, rgb, button_names):
+def check_status(name, delay, x, y, rgb, button_name):
   print(f"checando {name}...")
   pg.sleep(delay)
   if pg.pixelMatchesColor(x, y, rgb):
-      for button in button_names:
-          pg.press(button)
-          print(F"Pressed  {button}")
-          time.sleep(0.2)
+    pg.press(button_name)
 
 def check_battle():
     return pg.locateOnScreen("imgs/battle2.png", region=constants.REGION_BATTLE)
+
+def check_anchor():
+    box = pg.locateOnScreen("imgs/Anchfloor1.png", confidence=0.8)
+    if box:
+       pg.moveTo(1893, 12)
+       pg.click()
+       pg.moveTo(1893, 12)
+       pg.click(1105, 550)
+       return False
+    return True
        
     
